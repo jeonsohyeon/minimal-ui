a703e869b6100285b276d4a8f92409d7789bd397
jeonsh3 <jeonsh3cp@gmail.com>
2020-10-22 19:34:47 +0900

    project init setting

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,9 @@
-import commonjs from 'rollup-plugin-commonjs';
 import resolve from 'rollup-plugin-node-resolve';
 import babel from 'rollup-plugin-babel';
 import postcss from 'rollup-plugin-postcss';
-import pkg from './package.json';
 import url from 'rollup-plugin-url';
-import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 import { terser } from 'rollup-plugin-terser';
-
+import commonjs from '@rollup/plugin-commonjs';
 const extensions = ['.js', '.jsx', '.ts', '.tsx'];
 
 // babel-preset-react-app를 사용한다면 BABEL_ENV를 필수로 설정
@@ -14,29 +11,50 @@ process.env.BABEL_ENV = 'production';
 
 export default {
   input: './src/index.ts',
+  output: [
+    {
+      file: package.module,
+      format: 'es',
+    },
+  ],
   plugins: [
-    peerDepsExternal(),
-    resolve({ extensions }),
-    commonjs({
-      include: 'node_modules/**',
+    postcss({
+      modules: true,
     }),
+    url(),
     babel({
       extensions,
       include: ['src/**/*'],
       runtimeHelpers: true,
       presets: [['react-app', { flow: false, typescript: true }]],
+
+      exclude: 'node_modules/**',
     }),
+    resolve({ extensions }),
     postcss({
       extract: true,
       modules: true,
     }),
     terser(),
     url(),
+
+    commonjs({
+      dynamicRequireTargets: [
+        // include using a glob pattern (either a string or an array of strings)
+        'node_modules/logform/*.js',
+        // exclude files that are known to not be required dynamically, this allows for better optimizations
+        '!node_modules/logform/index.js',
+        '!node_modules/logform/format.js',
+        '!node_modules/logform/levels.js',
+        '!node_modules/logform/browser.js',
+      ],
+      ignoreGlobal: true,
+      include: /\/node_modules\//,
+      namedExports: {
+        react: Object.keys(require('react')),
+        'react-is': Object.keys(require('react-is')),
+      },
+    }),
   ],
-  output: [
-    {
-      file: pkg.module,
-      format: 'es',
-    },
-  ],
+  external: ['lodash'],
 };
